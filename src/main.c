d60d0a4e4c191f3724d5ff9f131e09d3a74bf8bd
Rynskyi <rynskyi@gmail.com>
2023-08-11 15:46:51 +0300

    rename some defines

--- src/main.c
+++ src/main.c
@@ -6,34 +6,33 @@
 #include <nrf24.h>
 
 
-#define BOOTLOADER_ADDRESS      0x0400
+#ifndef RWWSRE              // Bug in AVR libc:
+    #define RWWSRE CTPB     // RWWSRE is not defined on ATTinys, use CTBP instead
+#endif
+
+
+#define APP_ADDR                0x0000
+#define BOOTLOADER_ADDR         0x0400
 #define PKG_MAGIK               0x7777
 #define PKG_SIZE                4
 #define RF24_CHANNEL            0x77
 #define RF24_PIPE               {0x31, 0x31, 0x31, 0x31, 0x31}
 
+#define EEPROM_ADDR             (uint16_t *)(E2END - 2)  // last 2 bytes in EEPROM
+#define RJMP_OPCODE             (uint16_t)0xC000
+#define BOOTLOADER_RJMP         RJMP_OPCODE + (BOOTLOADER_ADDRESS / 2) - 1
 
-#ifndef RWWSRE              // Bug in AVR libc:
-    #define RWWSRE CTPB     // RWWSRE is not defined on ATTinys, use CTBP instead
-#endif
-
-#define BOOTLOADER_RJMP         0xC000 + (BOOTLOADER_ADDRESS / 2) - 1
 #define bit_set(reg, bit)       reg |= (1 << bit)
 #define bit_clr(reg, bit)       reg &= ~(1 << bit)
-#define NOOP                    __asm__ volatile ("nop")
-#define JUMP(addr)              __asm__ volatile ("rjmp %0" :: "i" (addr))
+#define noop()                  __asm__ volatile ("nop")
+#define rjmp(addr)              __asm__ volatile ("rjmp %0" :: "i" (addr))
 #define ijmp(addr)              asm volatile ("ijmp" :: "z" (addr));
 
 #define TIMER0_OVRFLOW          (TIFR0 & (1 << OCF0A))
-#define init_timer()                            \
-    {                                           \
-        TCCR0B = (1 << CS12) | (1 << CS10);     \
-        OCR0A = 98;                             \
-    }
-// OCR0A: ((F_CPU / (PRESCALER * 1000)) * TIME_MS)
 
 
-void write_page(uint32_t adress, uint8_t *buffer)
+// Write one page to Flash memory
+void write_page(uint16_t adress, uint8_t *buffer)
 {
     uint16_t offset, word;
     // Erase the memory
@@ -74,11 +73,12 @@ int main()
     DDRB = (1 << 1);   // pin output: LDO (B1)
     bit_set(PORTB, 1); // power on RF24 module
 
-    // Wait for power on Rf24
-    _delay_ms(10);
-
     // Init timer for timeout check
-    init_timer();
+    TCCR0B = (1 << CS12) | (1 << CS10);
+    OCR0A = 98;         // ~100ms (F_CPU / (PRESCALER * 1000)) * MS
+
+    // Wait for power on RF24
+    _delay_ms(10);
 
     // Init RF24 Radio module
     uint8_t mac[5] = RF24_PIPE; // TODO: add to progmem
@@ -90,7 +90,7 @@ int main()
     uint8_t started = 0, i = 0;
     uint8_t page[SPM_PAGESIZE] = {0};
     int16_t length = 0;
-    uint16_t address = 0x0000;
+    uint16_t address = APP_ADDR;
     
     while (!TIMER0_OVRFLOW)
     {
@@ -116,11 +116,11 @@ int main()
                 length -= PKG_SIZE;
                 i += PKG_SIZE;
 
-                // Patch reset vector in first word (rjmp to Bootloader)
+                // Patch app reset vector and save original to eeprom
                 if (i == PKG_SIZE && address == 0) {
-                    uint16_t app_addr = *(uint16_t*)page - 0xC000 + 1;
-                    eeprom_write_word(0x00, app_addr);
-                    *(uint16_t*)page = (uint16_t)BOOTLOADER_RJMP;
+                    uint16_t app_addr = *(uint16_t*)page - RJMP_OPCODE + 1;
+                    eeprom_write_word(EEPROM_ADDR, app_addr);
+                    *(uint16_t*)page = RJMP_OPCODE + (BOOTLOADER_ADDR / 2) - 1;
                 }
 
                 // Write page when it's full or when it's the last package
@@ -150,6 +150,6 @@ int main()
         bit_clr(PORTA, 7);
     }
 
-    uint16_t app = eeprom_read_word(0x00);
+    uint16_t app = eeprom_read_word(EEPROM_ADDR);
     ijmp(app);
 }
